b91f73df96d8368483feb37ca3a1e5e204ae38d1
DiegoFLM <diego.ledesma@correounivalle.edu.co>
2023-05-11 19:48:10 -0500

    f-strings separated for readability

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,4 +11,7 @@ subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 
-print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
+print(
+    f"{MESSAGE_COLOR}The beginning of your destiny is defined now! "
+    f"Create and have fun!{RESET_ALL}"
+)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -11,20 +11,30 @@ RESET_ALL = "\x1b[0m"
 
 # Check if the project_slug starts with a number
 if project_slug[0].isdigit():
-    print(f"{ERROR_COLOR}ERROR: {project_slug=} is not a valid name for this template. Project names should not start with a number.{RESET_ALL}")
+    print(
+        f"{ERROR_COLOR}ERROR: {project_slug=} is not a valid name for this template. "
+        f"Project names should not start with a number.{RESET_ALL}"
+    )
     sys.exit(1)
 
 
 # Check if the project_slug contains invalid characters
 valid_characters = set("abcdefghijklmnopqrstuvwxyz0123456789_-")
 if not all(char in valid_characters for char in project_slug):
-    print(f"{ERROR_COLOR}ERROR: {project_slug=} is not a valid name for this template. Project names should only contain lowercase letters, numbers, hyphens, and underscores.{RESET_ALL}")
+    print(
+        f"{ERROR_COLOR}ERROR: {project_slug=} is not a valid name for this template. "
+        f"Project names should only contain lowercase letters, numbers, hyphens, "
+        f"and underscores.{RESET_ALL}"
+    )
     sys.exit(1)
 
 
 # Check if the project_slug is already a directory
 if os.path.isdir(project_slug):
-    print(f"{ERROR_COLOR}ERROR: A directory with the name '{project_slug}' already exists in the current directory. Please choose a different project name.{RESET_ALL}")    
+    print(
+        f"{ERROR_COLOR}ERROR: A directory with the name '{project_slug}' already "
+        f"exists in the current directory. Please choose a different project name.{RESET_ALL}"
+    )    
     sys.exit(1)
 
 print(f"{MESSAGE_COLOR}Let's do it! You're going to create something awesome!")
