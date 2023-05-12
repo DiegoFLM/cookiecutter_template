fb3f42174ac69db3c0f8a72c4500d72cbee051d4
DiegoFLM <diego.ledesma@correounivalle.edu.co>
2023-05-11 21:08:24 -0500

    MIT License added with year and name. A function for putting the current year on the License section of README.md was included in post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,27 @@
 import os
+import datetime
 import subprocess
 
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
+
+
+def replace_year_in_files():
+    """Replace the placeholder for the year in files with the current year."""
+    current_year = str(datetime.datetime.now().year)
+    for dirpath, dirnames, filenames in os.walk('.'):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            with open(filepath, 'r') as file:
+                filedata = file.read()
+            if '[[year]]' in filedata:
+                filedata = filedata.replace('[[year]]', current_year)
+                with open(filepath, 'w') as file:
+                    file.write(filedata)
+
+replace_year_in_files()
+
+
 
 print(f"{MESSAGE_COLOR}Almost done!")
 print(f"Initializing a git repository...{RESET_ALL}")
